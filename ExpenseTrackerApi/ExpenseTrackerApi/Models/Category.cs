34ecb2eb665f434314f2e80168de6be807efd7c4
SpectroXRL <spectrothedev@gmail.com>
2025-05-31 03:11:09 -0500

    Basic CRUD request setup Appropriate models created

---
+++ ExpenseTrackerApi/ExpenseTrackerApi/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTrackerApi.Models
+{
+    public class Category
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+    }
+}
